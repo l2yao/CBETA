36df72770f1ab4a73bcdbc5d39b276f27b8ff8a8
l2yao <yl1984108@hotmail.com>
2013-07-09 15:15:04 -0400

    remove full screen activity

--- android/gen/com/cbeta/sutra/BuildConfig.java
+++ android/gen/com/cbeta/sutra/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.sutra.cbeta;
+package com.cbeta.sutra;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
