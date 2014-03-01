14a52b97db22cb4797e2503e5e83cfcf7ee96b3d
Long Yao <yl1984108@gmail.com>
2014-03-01 17:31:05 -0500

    remove local html, use remote cbeta mobile site

--- android/src/com/cbeta/sutra/MainActivity.java
+++ android/src/com/cbeta/sutra/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 		WebView webview = (WebView)this.findViewById(R.id.webView1);
 		webview.setClickable(true);
 		webview.getSettings().setJavaScriptEnabled(true);
-		webview.loadUrl("file:///android_asset/html/index.htm");
+		webview.loadUrl("http://tripitaka.cbeta.org/mobile/");
 	}
 
 	@Override
