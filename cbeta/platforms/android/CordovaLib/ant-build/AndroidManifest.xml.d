55dd5e5b3856d54a94c668e381d630d0ff79f9bd
l2yao <yl1984108@hotmail.com>
2014-06-09 22:09:00 -0400

    add cordova code for cbeta

---
+++ cbeta/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/long/Documents/CBETA-android/cbeta/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/long/Documents/CBETA-android/cbeta/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ cbeta/platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/long/Documents/CBETA-android/cbeta/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
